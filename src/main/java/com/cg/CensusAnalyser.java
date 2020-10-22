dfc702d3a969b7576b28f0854e6ad43b367af3ed
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-22 20:45:11 +0530

    [Arijit]Added : Reporting State Census Data In Json Format According To State

--- src/main/java/com/cg/CensusAnalyser.java
+++ src/main/java/com/cg/CensusAnalyser.java
@@ -5,10 +5,12 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.StreamSupport;
 import com.cg.csvbuilder.*;
+import com.google.gson.Gson;
 
 public class CensusAnalyser {
 	
@@ -25,7 +27,7 @@ List<CsvStateCensus> csvStateCensusList;
 	}
 
 		try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
-			com.cg.commoncsv.ICsvBuilder csvBuilder = com.cg.commoncsv.CsvBuilderFactory.createCsvBuilder();
+			com.cg.csvbuilder.ICsvBuilder csvBuilder = com.cg.csvbuilder.CsvBuilderFactory.createCsvBuilder();
 			csvStateCensusList = csvBuilder.getCsvFileIList(reader, CsvStateCensus.class);
 			return csvStateCensusList.size();
 		} catch (IOException e) {
@@ -35,7 +37,7 @@ List<CsvStateCensus> csvStateCensusList;
 		}
 		catch(RuntimeException e) {
 			throw new CensusAnalyserException("File internal data not proper", CensusAnalyserException.ExceptionType.INTERNAL_ISSUE);
-		} catch (com.cg.commoncsv.CsvException e) {
+		} catch (com.cg.csvbuilder.CsvException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
@@ -46,7 +48,7 @@ List<CsvStateCensus> csvStateCensusList;
 	}
 
 		try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))){
-			com.cg.commoncsv.ICsvBuilder csvBuilder = com.cg.commoncsv.CsvBuilderFactory.createCsvBuilder();
+			com.cg.csvbuilder.ICsvBuilder csvBuilder = com.cg.csvbuilder.CsvBuilderFactory.createCsvBuilder();
 			csvStateCensusList = csvBuilder.getCsvFileIList(reader, CsvStateCode.class);
 			return csvStateCensusList.size();
 		} catch (IOException e) {
@@ -56,7 +58,7 @@ List<CsvStateCensus> csvStateCensusList;
 		}
 		catch(RuntimeException e) {
 			throw new CensusAnalyserException("File internal data not proper", CensusAnalyserException.ExceptionType.INTERNAL_ISSUE);
-		} catch (com.cg.commoncsv.CsvException e) {
+		} catch (com.cg.csvbuilder.CsvException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
@@ -65,5 +67,28 @@ List<CsvStateCensus> csvStateCensusList;
 		Iterable<E> csvIterable = () -> iterator;
 		int numOfEntries = (int)StreamSupport.stream(csvIterable.spliterator(), false).count();
 		return numOfEntries;
+	}
+	
+	public String getStateWiseSortedCensusData() throws CensusAnalyserException {
+		if(csvStateCensusList == null || csvStateCensusList.size() == 0) {
+			throw new CensusAnalyserException("File is empty", CensusAnalyserException.ExceptionType.NO_DATA);
+		}
+			Comparator<CsvStateCensus> censusComparator = Comparator.comparing(census -> census.state);
+			this.sort( censusComparator);
+			String sortedStateCensus = new Gson().toJson(csvStateCensusList);
+			return sortedStateCensus;
+	}
+
+	private void sort(Comparator<CsvStateCensus> censusComparator) {
+		for(int i = 0;i < csvStateCensusList.size()-1; i++) {
+			for(int j = 0; j < csvStateCensusList.size()-1-i; j++) {
+				CsvStateCensus census1 = csvStateCensusList.get(j);
+				CsvStateCensus census2 = csvStateCensusList.get(j+1);
+				if(censusComparator.compare(census1, census2) > 0) {
+					csvStateCensusList.set(j, census2);
+					csvStateCensusList.set(j+1, census1);
+				}
+			}
+		}
 	}
 }
